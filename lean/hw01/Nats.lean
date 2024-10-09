b21ac1f751e6c44234bedbb1237d521deee0a391
Jacpc <tonho_kun@ymail.com>
2024-10-09 16:47:49 -0300

    add, mul, pow e NM-idL

--- lean/hw01/Nats.lean
+++ lean/hw01/Nats.lean
@@ -4,14 +4,48 @@
 inductive Nats
 | O : Nats
 | S : Nats → Nats
+deriving Repr
+
+open Nats
 
 def add : Nats → Nats → Nats
-| n, Nats.O => n
-| n, Nats.S m => Nats.S (add n m)
+| n, O => n
+| n, S m => S (add n m)
+
+def mul : Nats → Nats → Nats
+| n, O => O
+| n, S m => add n (mul n m)
+
+def pow : Nats → Nats → Nats
+| n, O => S O
+| n, S m => mul n (pow n m)
 
 instance : HAdd Nats Nats Nats where
   hAdd := add
+instance : HMul Nats Nats Nats where
+  hMul := mul
+instance : HPow Nats Nats Nats where
+  hPow := pow
 --==================================
 -- Axiomas:
 --==================================
 axiom NA_Ass (a b c: Nats) : (a + b) + c = a + (b + c)
+axiom NA_idR (a : Nats) : a = a + O
+--axiom NA_invR (a : Nats) : O = a + (-a)
+axiom NA_Com (a b : Nats) : a + b = b + a
+
+axiom NM_Ass (a b c: Nats) : (a * b) * c = a * (b * c)
+axiom NM_idR (a : Nats) : a = a * (S O)
+axiom NM_Com (a b : Nats) : a * b = b * a
+
+axiom NE_idR (a : Nats) : a = a ^ (S O)
+
+
+--==================================
+-- Lemmas da Esquerda:
+--==================================
+theorem NM_idL: ∀ a : Nats, a = (S O) * a := by
+  intro a
+  have h: a = a * (S O) := NM_idR a
+  rw [NM_Com a (S O)] at h
+  exact h
