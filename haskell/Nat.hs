a3fbc21c2c81f52afeefdf6e729667c22dad08a3
Jacpc <tonho_kun@ymail.com>
2024-10-17 13:12:05 -0300

    Renomeando

---
+++ haskell/Nat.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE GADTs, NoImplicitPrelude #-}
+module Nat where
+
+    data Nat where
+        O :: Nat
+        S :: Nat -> Nat
+
+    (+) :: Nat -> Nat -> Nat
+    n + O = n
+    n + (S m) = S (n + m)
+
+    (*) :: Nat -> Nat -> Nat
+    n * O = O
+    n * (S m) = n + (n * m)
+
+    (^) :: Nat -> Nat -> Nat
+    n ^ O = S O
+    n ^ (S m) = n * (n ^ m)
+
+    double :: Nat -> Nat
+    double n = n * S (S O)
+
+    pd :: Nat -> Nat
+    pd O = O
+    pd (S n) = n
+
+    fact :: Nat -> Nat
+    fact O = S O
+    fact (S n) = fact n * S n
+
+    fib :: Nat -> Nat
+    fib (S (S n)) = fib (S n) + fib n
+    fib n = n
+    
+    min :: (Nat, Nat) -> Nat
+    min (n, O) = O
+    min (O, n) = O
+    min (S n, S m) = S (min (n, m))
+
+    max :: (Nat, Nat) -> Nat
+    max (n, O) = n
+    max (O, n) = n
+    max (S n, S m) = S (max (n, m))
+
+    --quot :: (Nat, Nat) -> Nat
+    
+    --rem :: (Nat, Nat) -> Nat
+
+    --gcd:: (Nat, Nat) -> Nat
+
+    --lcm:: (Nat, Nat) -> Nat
