e5ce88f08e70ecd135ff35537ffa0a20514f8786
Jacpc <tonho_kun@ymail.com>
2024-10-18 09:55:20 -0300

    ++

--- haskell/List.hs
+++ haskell/List.hs
@@ -4,3 +4,7 @@ module List where
     data List α where
         Nil :: List α
         Cons :: α -> List α -> List α
+
+    (++) :: [a] -> [a] -> [a]
+    [] ++ xs = xs
+    (x:xs) ++ ys = x:(xs ++ ys)
